318164cb071310202a7574ae84c3f78e91d8b378
ssendhil <selvamsendhil@gmail.com>
2019-07-01 17:07:13 -0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd("~/Desktop/Coursera/4_Exploratory_Data_Analysis/Project 1")
+
+plot1_data <- read.table("household_power_consumption.txt", header = T, 
+                         sep = ";", na.strings = "?")
+
+# Fix dates
+plot1_data$Date <- as.Date(plot1_data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+plot1_data_subset <- subset(plot1_data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Plot 1
+hist(plot1_data_subset$Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+
+# Save file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
